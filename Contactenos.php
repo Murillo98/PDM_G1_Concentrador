87f126f5d277d92a404d6662ce75582240bec607
Murillo98 <86999104+Murillo98@users.noreply.github.com>
2021-07-06 00:37:48 -0500

    Add files via upload

---
+++ Contactenos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Pag Principal</title>
+</head>
+
+<body>
+<p>Bienvenidos a la web del Grupo 1 del curso de PDM de la PUCP</p>
+<p><a href="index.php">Inicio</a> | <a href="Contactenos.php">Contactenos</a> | <a href="Contactenos.php">Ingresar</a></p>
+<p>Somos alumnos de la PUCP</p>
+</body>
+</html>
